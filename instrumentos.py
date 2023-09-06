3d7cb96b39fdc645a18099ec0079e63395277079
skullfear <141983125+skullfear@users.noreply.github.com>
2023-09-06 07:42:47 -0500

    Update instrumentos.py

--- instrumentos.py
+++ instrumentos.py
@@ -1,14 +1,37 @@
 class Instrumento:
-    def __init__(self, tipo, tocar, afinar):
-        self.tipo = tipo
-        self.tocar = tocar
-        self. afinar = afinar
-
-
-    def mostrar_informacion(self):
-        return (self.tipo, self.tocar, self.afinar) 
+    def afinar(afinar):
+        return "Instrumento no afinable"
     
-class Musico(Instrumento):
-    def saludar(self):
-        return "hola"
+    def tocar(tocar):
+        return "tocando instrumento"
+        
+class Guitarra(Instrumento):
+    def afinar(self):
+        return "afinando guitarra"
+    def tocar(self):
+        return "tocando Guitarra"
+    
+class Maracas(Instrumento):
+    def afinar(self):
+        return "no se pueden afinar las maracas"
+    def tocar(self):
+        return "tocando Maracas"
+    
+class Charrasca(Instrumento):
+    def afinar(self):
+        return "no se pueden afinar las Charrascas"
+    def tocar(self):
+        return "tocando Charrasca"
+    
+class Bajo(Instrumento):
+    def afinar(self):
+        return "afinando Bajo"
+    def tocar(self):
+        return "tocando Bajo"
+     
+class Violin(Instrumento):
+    def afinar(self):
+        return "afinando violin"
+    def tocar(self):
+        return "tocando violin"
         
