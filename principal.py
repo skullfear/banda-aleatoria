1afe395164440b1c91fc0668d9985510b0ea5af5
skullfear <141983125+skullfear@users.noreply.github.com>
2023-09-06 07:43:03 -0500

    Update principal.py

--- principal.py
+++ principal.py
@@ -1,13 +1,7 @@
 from banda import *
 
-t = Banda()
+b = Banda()
+b.crear_banda()
 
-t.agregar_instrumento(Musico("guitarra", "tocando guitarra", "afinando guitarra"))
-t.agregar_instrumento(Musico("bateria", "tocando bateria", "afinando bateria"))
-t.agregar_instrumento(Musico("piano", "tocando piano", "afinando piano"))
-t.agregar_instrumento(Musico("bajo", "tocando bajo", "afinando bajo"))
-t.agregar_instrumento(Musico("flauta", "tocando flauta", "afinando flauta"))
-
-for i in range(3):
-    a = t.entregar_instrumento()
-    print(a.saludar(), a.mostrar_informacion())
+b.afinar_banda()
+b.tocar_banda()
