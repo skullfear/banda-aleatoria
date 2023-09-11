3abfdaafde1959a65c2b04b698c843f433b30f37
skullfear <141983125+skullfear@users.noreply.github.com>
2023-09-10 23:39:10 -0500

    Update imagen.py

--- imagen.py
+++ imagen.py
@@ -1,23 +1,69 @@
-from banda import *
-import pygame
-from pygame.locals import *
+import tkinter as tk
+from tkinter import ttk
 
-pygame.init()
+class Instrumento:
+    def afinar(self):
+        return "Instrumento no afinable"
 
-width, height = 800, 600
+    def tocar(self):
+        return "Tocando instrumento"
 
-screen = pygame.display.set_mode((width, height))
+class Guitarra(Instrumento):
+    def afinar(self):
+        return "Afinando guitarra"
 
-instrument_image = pygame.image.load('guitarra.png')
+    def tocar(self):
+        return "Tocando guitarra"
 
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            running = False
+class Maracas(Instrumento):
+    def tocar(self):
+        return "Tocando maracas"
 
-    screen.fill((255, 255, 255))
+class Charrasca(Instrumento):
+    def tocar(self):
+        return "Tocando charrasca"
 
-    screen.blit(instrument_image, (100, 100)) 
+class Bajo(Instrumento):
+    def afinar(self):
+        return "Afinando bajo"
 
+    def tocar(self):
+        return "Tocando bajo"
+
+class Violin(Instrumento):
+    def afinar(self):
+        return "Afinando violín"
+
+    def tocar(self):
+        return "Tocando violín"
+
+def mostrar_info_instrumento():
+    instrumento = combo_instrumento.get()
+    if instrumento == "Guitarra":
+        resultado_label.config(text=Guitarra().afinar() + "\n" + Guitarra().tocar())
+    elif instrumento == "Maracas":
+        resultado_label.config(text=Maracas().tocar())
+    elif instrumento == "Charrasca":
+        resultado_label.config(text=Charrasca().tocar())
+    elif instrumento == "Bajo":
+        resultado_label.config(text=Bajo().afinar() + "\n" + Bajo().tocar())
+    elif instrumento == "Violín":
+        resultado_label.config(text=Violin().afinar() + "\n" + Violin().tocar())
+    else:
+        resultado_label.config(text="Instrumento no encontrado")
+
+ventana = tk.Tk()
+ventana.title("Aplicación de Instrumentos")
+
+instrumentos = ["Guitarra", "Maracas", "Charrasca", "Bajo", "Violín"]
+combo_instrumento = ttk.Combobox(ventana, values=instrumentos)
+combo_instrumento.pack()
+
+mostrar_button = tk.Button(ventana, text="Mostrar Info", command=mostrar_info_instrumento)
+mostrar_button.pack()
+
+resultado_label = tk.Label(ventana, text="")
+resultado_label.pack()
+
+ventana.mainloop()
 
